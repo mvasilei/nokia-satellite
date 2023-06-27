6ede99601c619b74bc6f18fad0c0ee7c223291a9
mvasilei <53435274+mvasilei@users.noreply.github.com>
2023-06-27 12:03:32 +0100

    Update svcheck.py
    Change from xls to csv

--- svcheck.py
+++ svcheck.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
-import sys, signal, os, xlsxwriter, subprocess, re
+import sys, signal, os, csv, subprocess, re
 from optparse import OptionParser
 
 def signal_handler(sig, frame):
     print('Exiting gracefully Ctrl-C detected...')
     sys.exit()
 
-def vprn_lookup(vprn_id, worksheet):
+def vprn_lookup(vprn_id, csvwriter):
     policy_regex = re.compile(r'vrf-[ei][mx]port.*', re.MULTILINE) #match import export policies
     vprn_regex = re.compile(r'(^\s{8}vprn\s' + re.escape(vprn_id) + '\s(\n|.)*?^\s{8}exit)', re.MULTILINE) #match entire vprn configuration
     bgp_group_regex = re.compile(r'^\s{12}bgp(\n|.)*?^\s{12}exit', re.MULTILINE) #match entire bgp configuration
@@ -33,33 +33,32 @@ def vprn_lookup(vprn_id, worksheet):
                         policies = re.findall(policy_regex, match.group(1))
 
                         if policies:
-                            worksheet.write(row, 0, vprn_id)
-                            worksheet.write(row, 1, device)
+                            mc_mx_routes =''
+                            mx_routes = ''
+                            bgp_policy = ''
+                            import_policy = ''
+                            export_policy = ''
 
                             mxroutes = re.findall(r'maximum-routes.+|mc-maximum-routes.+', match.group(1))
 
                             for mxroute in mxroutes:
                                 if 'mc' in mxroute:
-                                    worksheet.write(row, 5, mxroute.split()[1] + '+' + mxroute.split()[3])
+                                    mc_mx_routes = mxroute.split()[1] + '+' + mxroute.split()[3]
                                 else:
-                                    worksheet.write(row, 4, mxroute.split()[1] + '+' + mxroute.split()[4])
+                                    mx_routes =  mxroute.split()[1] + '+' + mxroute.split()[4]
 
                             for bgp in bgp_group_regex.finditer(match.group(1)):
                                 for bgp_group in re.finditer(r'group.*', bgp.group()):
-                                    worksheet.write(row, 6, bgp_group.group().split(' ',1)[-1])
+                                    bgp_policy = bgp_group.group().split(' ',1)[-1]
 
                             for policy in policies:
                                 if 'import' in policy:
-                                    worksheet.write(row, 2, policy.split(' ',1)[-1])
+                                    import_policy = policy.split(' ',1)[-1]
                                 else:
-                                    worksheet.write(row, 3, policy.split(' ',1)[-1])
+                                    export_policy = policy.split(' ',1)[-1]
+
+                            csvwriter.writerow([vprn_id, device, import_policy, export_policy, mx_routes, mc_mx_routes, bgp_policy])
                             row += 1
-
-def open_xls_to_write(svc):
-    book = xlsxwriter.Workbook('EPE_SVC_'+svc+'_Policies.xlsx')
-    svc_sheet = book.add_worksheet('SVC')
-
-    return book, svc_sheet
 
 def main():
     usage = 'usage: %prog options'
@@ -82,17 +81,13 @@ def main():
     except ValueError:
         print("The Service ID provided doesn't have the correct format")
 
-    book, svc_sheet = open_xls_to_write(options.svc)
-    svc_sheet.write(0, 0, 'Service ID')
-    svc_sheet.write(0, 1, 'Device')
-    svc_sheet.write(0, 2, 'Import Policy')
-    svc_sheet.write(0, 3, 'Export Policy')
-    svc_sheet.write(0, 4, 'Max Routes')
-    svc_sheet.write(0, 5, 'Max Multicast Routes')
-    svc_sheet.write(0, 6, 'BGP Group')
-    vprn_lookup(options.svc, svc_sheet)
-    book.close()
-    print ('Results file ' + 'EPE_SVC_'+options.svc+'_Policies.xlsx')
+    with open('EPE_SVC_'+options.svc+'_Policies.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',',
+                               quotechar='|')
+        csvwriter.writerow(['Service ID', 'Device', 'Import Policy', 'Export Policy', 'Max Routes',
+                            'Max Multicast Routes', 'BGP Group'])
+        vprn_lookup(options.svc, csvwriter)
+        print ('Results file ' + 'EPE_SVC_'+options.svc+'_Policies.csv')
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)  # catch ctrl-c and call handler to terminate the script
